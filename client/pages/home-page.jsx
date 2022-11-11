42577f07fe0d5bab5624f66a21710c1c13462fcd
Garrett Quathamer <gquathamer@gmail.com>
2022-11-11 05:12:05 +0000

    added webpack plugin to include odds api key, started cleaning data structure coming back from odds api

--- client/pages/home-page.jsx
+++ client/pages/home-page.jsx
@@ -4,18 +4,37 @@ import Oddsbar from '../components/odds-bar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
 
 export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      odds: ''
+      odds: []
     };
   }
 
   componentDidMount() {
-    fetch('https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds?apiKey=' + process.env.ODDS_API_KEY + '&regions=us&oddsFormat=american&markets=h2h,spreads,totals&bookmakers=bovada')
+    fetch(`https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds?apiKey=${process.env.API_KEY}&regions=us&oddsFormat=american&markets=h2h,spreads,totals&bookmakers=bovada`)
       .then(response => response.json())
+      .then(response => {
+        const markets = [];
+        for (let i = 0; i < response.length; i++) {
+          const marketsObject = {};
+          marketsObject.homeTeam = response[i].home_team;
+          marketsObject.awayTeam = response[i].away_team;
+          const odds = response[i].bookmakers[0].markets;
+          for (let j = 0; j < odds.length; j++) {
+            if (odds[j].key === 'spreads') {
+              marketsObject.spreads = odds[j].outcomes;
+            } else {
+              marketsObject.h2h = odds[j].outcomes;
+            }
+          }
+          markets.push(marketsObject);
+        }
+        return markets;
+      })
       .then(response => {
         this.setState({
           odds: response
@@ -29,15 +48,45 @@ export default class HomePage extends React.Component {
         <Navigation />
         <Oddsbar />
         <Container fluid="md" className="mt-5">
-          <Row className="justify-content-center">
-            <Col md={6} sm={9}>
-              {
-                this.state.odds.map(elem => {
-                  return <h1 key={elem.id}>elem.home_team</h1>;
-                })
-              }
-            </Col>
-          </Row>
+          {
+            this.state.odds.map(elem => {
+              return (
+                <Row key={elem.id} className="justify-content-center">
+                  <Col key={elem.id} md={6} sm={9}>
+                    <Table>
+                      <thead>
+                        <tr>
+                          <th>Team</th>
+                          <th>Spread</th>
+                          <th>Line</th>
+                          <th>Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{elem.awayTeam}</td>
+                          <td>test</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <td>{elem.homeTeam}</td>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td colSpan={2}>Larry the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Col>
+                </Row>
+              );
+            })
+          }
         </Container>
       </>
     );
