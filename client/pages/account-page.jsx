2a2fc4920b37bb212cf336b0139dbc74df9faecb
Garrett Quathamer <gquathamer@gmail.com>
2023-01-22 16:05:30 +0000

    bet history now rendering one table with multiple rows, need to add bet outcome and color

--- client/pages/account-page.jsx
+++ client/pages/account-page.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navigation from '../components/navbar';
 import Oddsbar from '../components/odds-bar';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
 import AppContext from '../lib/app-context';
 
@@ -36,37 +35,34 @@ export default class AccountPage extends React.Component {
         <Navigation />
         <Oddsbar />
         <Container>
-          {
-            this.state.betHistory.map(elem => {
-              return (
-                <Row key={elem.betId} className="justify-content-center">
-                  <Table bordered className='table' key={elem.betId} id={elem.betId}>
-                    <thead>
-                      <tr className="td-no-wrap td-quarter">
-                        <th />
-                        <th>Date</th>
-                        <th>Bet</th>
-                        <th>Amount</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr className='td-no-wrap td-quarter'>
-                        <td rowSpan="2" className="align-middle text-center">{new Date(elem.gameStart).toLocaleDateString()}</td>
-                        <td className="">
-                          {elem.awayTeam} @ {elem.homeTeam}
-                          <br />
-                          {elem.winningTeam} {elem.points} ({elem.price})
-                        </td>
-                        <td className="">
-                          {elem.betAmount}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Row>
-              );
-            })
-          }
+          <Table bordered className='table mt-5' fluid="md">
+            <thead>
+              <tr className="td-no-wrap">
+                <th className='td-quarter align-middle text-center'>Date</th>
+                <th>Bet</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.betHistory.map(elem => {
+                  return (
+                    <tr className='td-no-wrap td-quarter' key={elem.id}>
+                      <td className="align-middle text-center">{new Date(elem.gameStart).toLocaleDateString()}</td>
+                      <td className="">
+                        {elem.awayTeam} @ {elem.homeTeam}
+                        <br />
+                        {elem.winningTeam} {elem.points} ({elem.price})
+                      </td>
+                      <td className="">
+                        {elem.betAmount}
+                      </td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </Table>
         </Container>
       </>
     );
